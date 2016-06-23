162d27bdb65ef9e51d329e3b8751676a202345f8
hama_du <dercll1020@gmail.com>
2016-06-24 00:52:43 +0900

    fixed line algorithm

--- src/utils/geometry.js
+++ src/utils/geometry.js
@@ -1,28 +1,37 @@
 export default () => {
   const lineToDots = (fx, fy, tx, ty) => {
     const dots = [];
-    if (fx == tx) {
+    if (tx < fx) {
+      return lineToDots(tx, ty, fx, fy);
+    } else if (fx == tx) {
       while (fy != ty) {
         dots.push([Math.floor(fx), Math.floor(fy)]);
         fy += (ty - fy >= 1) ? 1 : -1;
       }
-    } else {
-      const diff = Math.abs((ty - fy) / (tx - fx));
+      dots.push([fx, fy]);
+    } else if (fy == ty) {
       while (fx != tx) {
-        const tty = fy + diff * ((ty >= fy) ? 1 : -1);
+        dots.push([Math.floor(fx), Math.floor(fy)]);
+        fx += 1;
+      }
+      dots.push([fx, fy]);
+    } else {
+      const diff = (ty - fy) / (tx - fx + 1);
+      while (fx <= tx) {
+        const tty = fy + diff;
         if (diff >= 0) {
           while (fy <= tty) {
-            dots.push([Math.floor(fx), Math.floor(fy)]);
+            dots.push([Math.floor(fx), Math.round(fy)]);
             fy += 1;
           }
         } else {
           while (fy >= tty) {
-            dots.push([Math.floor(fx), Math.floor(fy)]);
+            dots.push([Math.floor(fx), Math.round(fy)]);
             fy -= 1;
           }
         }
         fy = tty;
-        fx += (tx - fx >= 1) ? 1 : -1;
+        fx += 1;
       }
     }
     return dots;
